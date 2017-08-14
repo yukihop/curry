c4816322a0a0118d40ff333fb406093071d043f6
yukihop <mobacure.black@gmail.com>
2017-08-14 16:17:35 +0900

    Cleanup

--- curry.js
+++ curry.js
@@ -1,25 +1,21 @@
-const $comment = $('#comment');
-
 const $$ = document.getElementById.bind(document);
 
 const comment = $$('comment');
-
 comment.addEventListener('input', () => {
 	comment.style.height = '1px';
 	comment.style.height = (2 + comment.scrollHeight) + 'px';
 });
 
 const type = $$('type');
-
 type.addEventListener('click', () => {
 	const current = type.innerHTML;
 	let next = '＜Cute＞';
 	let nextClass = 'cute';
-	if (type.innerHTML.match(/Cute/i)) {
+	if (current.match(/Cute/i)) {
 		next = '＜Cool＞';
 		nextClass = 'cool';
 	}
-	if (type.innerHTML.match(/Cool/i)) {
+	if (current.match(/Cool/i)) {
 		next = '＜Passion＞';
 		nextClass = 'passion';
 	}
@@ -27,24 +23,20 @@ type.addEventListener('click', () => {
 	$$('name-container').className = nextClass;
 });
 
-document.body.addEventListener('focusout', () => {
-	loadFont();
-	console.log('pinya');
-});
-
 $$('portrait').addEventListener('click', () => {
 	$$('file-selector').click();
 });
 
 const selector = $$('file-selector');
-
 selector.addEventListener('change', () => {
 	const reader = new FileReader();
-	reader.onload = (e) => {
-		$$('portrait').style.backgroundImage = `url(${e.target.result})`;
+	reader.onload = ev => {
+		$$('portrait').style.backgroundImage = `url(${ev.target.result})`;
 	};
 	reader.readAsDataURL(selector.files[0]);
 });
+
+document.body.addEventListener('focusout', loadFont);
 
 function loadFont() {
 	try{
